e57917252b3cfc15c2e4ffd4d346ab2550024cdc
gagik894 <gagiksargsyan894@gmail.com>
2026-06-14 13:48:58 +0400

    feat: rename root project from JvTerm

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "Lattice"
+rootProject.name = "JvTerm"
 include(
     ":jvterm-core",
     ":jvterm-render-api",
